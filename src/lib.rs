2997275d34344ce16911a45abac7846cada93a8c
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-11 15:36:00 +0200

    First build setup with cbdindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+#[no_mangle]
+pub extern "C" fn add(a: i32, b: i32) -> i32 {
+    a+b
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(add(2, 2), 4);
     }
 }
