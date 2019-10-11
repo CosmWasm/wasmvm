0f9649f1600ec4766a37fb24311f2c11a48e4333
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-11 19:52:30 +0200

    Clean up api call types

--- api/api.go
+++ api/api.go
@@ -5,6 +5,9 @@ package api
 // #include "bindings.h"
 import "C"
 
+type i32 = C.int32_t
+
+
 func Add(a int32, b int32) int32 {
-	return (int32)(C.add((C.int32_t)(a), (C.int32_t)(b)))
+	return (int32)(C.add(i32(a), i32(b)))
 }
