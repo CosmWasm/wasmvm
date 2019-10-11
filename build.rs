bf69e76f40c710b5302be792ec75ff51e7617cd8
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-11 15:42:13 +0200

    Use cbindgen.toml

--- build.rs
+++ build.rs
@@ -3,9 +3,7 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::Builder::new()
-        .with_crate(crate_dir)
-        .generate()
+    cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
         .write_to_file("bindings.h");
 }
