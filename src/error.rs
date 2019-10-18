bd61fa498f2b167fd7022a0b90ae81c2e43b5eda
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-18 19:42:20 +0200

    Update rust side to go-rust-demo best practices

--- src/error.rs
+++ src/error.rs
@@ -1,42 +1,25 @@
-use errno;
 use errno::{set_errno, Errno};
-use std::cell::RefCell;
 use std::fmt::Display;
 
-use crate::memory::{release_vec, Buffer};
+use crate::memory::Buffer;
 
-thread_local! {
-    static LAST_ERROR: RefCell<Option<String>> = RefCell::new(None);
-}
-
-#[no_mangle]
-pub extern "C" fn get_last_error() -> Buffer {
-    let msg = take_last_error().unwrap_or_default();
-    release_vec(msg.into_bytes())
-}
-
-pub fn update_last_error(msg: String) {
-    LAST_ERROR.with(|prev| {
-        *prev.borrow_mut() = Some(msg);
-    });
+pub fn set_error(msg: String, errout: Option<&mut Buffer>) {
+    if let Some(mb) = errout {
+        *mb = Buffer::from_vec(msg.into_bytes());
+    }
     // Question: should we set errno to something besides generic 1 always?
     set_errno(Errno(1));
 }
 
-/// Retrieve the most recent error, clearing it in the process.
-pub fn take_last_error() -> Option<String> {
-    LAST_ERROR.with(|prev| prev.borrow_mut().take())
-}
-
-pub fn handle_c_error<T, E>(r: Result<T, E>) -> T
-where
-    T: Default,
-    E: Display,
+pub fn handle_c_error<T, E>(r: Result<T, E>, errout: Option<&mut Buffer>) -> T
+    where
+        T: Default,
+        E: Display,
 {
     match r {
         Ok(t) => t,
         Err(e) => {
-            update_last_error(e.to_string());
+            set_error(e.to_string(), errout);
             T::default()
         }
     }
--- src/memory.rs
+++ src/memory.rs
@@ -1,58 +1,63 @@
 use std::mem;
 use std::slice;
 
+// this frees memory we released earlier
+#[no_mangle]
+pub extern "C" fn free_rust(buf: Buffer) {
+    unsafe {
+        let _ = buf.consume();
+    }
+}
+
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub struct Buffer {
     pub ptr: *mut u8,
-    pub size: usize,
-}
-
-impl Buffer {
-    pub fn is_empty(&self) -> bool {
-        self.size == 0 || self.ptr.is_null()
-    }
+    pub len: usize,
+    pub cap: usize,
 }
 
 impl Default for Buffer {
     fn default() -> Self {
-        Buffer{
-            ptr: 0 as *mut u8,
-            size: 0,
+        Buffer{ptr: 0 as *mut u8, len: 0, cap: 0}
+    }
+}
+
+impl Buffer {
+    // read provides a reference to the included data to be parsed or copied elsewhere
+    // data is only guaranteed to live as long as the Buffer
+    // (or the scope of the extern "C" call it came from)
+    pub fn read(&self) -> Option<&[u8]> {
+        if self.is_empty() {
+            None
+        } else {
+            unsafe { Some(slice::from_raw_parts(self.ptr, self.len)) }
         }
     }
-}
 
-// this frees memory we released earlier
-#[no_mangle]
-pub extern "C" fn free_rust(buf: Buffer) {
-    if !buf.is_empty() {
-        unsafe {
-            let _ = Vec::from_raw_parts(buf.ptr, buf.size, buf.size);
+    // consume must only be used on memory previously released by from_vec
+    // when the Vec is out of scope, it will deallocate the memory previously referenced by Buffer
+    pub unsafe fn consume(self) -> Vec<u8> {
+        if self.is_empty() {
+            return Vec::new();
         }
+        let mut v = Vec::from_raw_parts(self.ptr, self.len, self.cap);
+        v.shrink_to_fit();
+        v
     }
-}
 
-pub fn read_buffer(b: &Buffer) -> Option<&'static [u8]> {
-    if b.is_empty() {
-        None
-    } else {
-        unsafe { Some(slice::from_raw_parts(b.ptr, b.size)) }
-    }
-}
-
-// this releases our memory to the caller
-pub fn release_vec(mut v: Vec<u8>) -> Buffer {
-    if v.len() == 0 {
-        return Buffer {
-            ptr: 0 as *mut u8,
-            size: 0,
+    // this releases our memory to the caller
+    pub fn from_vec(mut v: Vec<u8>) -> Self {
+        let buf = Buffer {
+            ptr: v.as_mut_ptr(),
+            len: v.len(),
+            cap: v.capacity(),
         };
+        mem::forget(v);
+        buf
     }
-    let buf = Buffer {
-        ptr: v.as_mut_ptr(),
-        size: v.len(),
-    };
-    mem::forget(v);
-    buf
+
+    pub fn is_empty(&self) -> bool {
+        self.ptr.is_null() || self.len == 0 || self.cap == 0
+    }
 }
