100ef0322852d9919b4a82267de223590f520928
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-02-26 17:37:03 +0100

    Use new 0.7 test contract

--- api/lib_test.go
+++ api/lib_test.go
@@ -45,7 +45,7 @@ func TestCreateAndGet(t *testing.T) {
 	cache, cleanup := withCache(t)
 	defer cleanup()
 
-	wasm, err := ioutil.ReadFile("./testdata/contract.wasm")
+	wasm, err := ioutil.ReadFile("./testdata/contract_0.7.wasm")
 	require.NoError(t, err)
 
 	id, err := Create(cache, wasm)
@@ -100,7 +100,7 @@ func TestInstantiate(t *testing.T) {
 	defer cleanup()
 
 	// create contract
-	wasm, err := ioutil.ReadFile("./testdata/contract.wasm")
+	wasm, err := ioutil.ReadFile("./testdata/contract_0.7.wasm")
 	require.NoError(t, err)
 	id, err := Create(cache, wasm)
 	require.NoError(t, err)
@@ -206,7 +206,7 @@ func requireOkResponse(t *testing.T, res []byte, expectedMsgs int) {
 }
 
 func createTestContract(t *testing.T, cache Cache) []byte {
-	wasm, err := ioutil.ReadFile("./testdata/contract.wasm")
+	wasm, err := ioutil.ReadFile("./testdata/contract_0.7.wasm")
 	require.NoError(t, err)
 	id, err := Create(cache, wasm)
 	require.NoError(t, err)
