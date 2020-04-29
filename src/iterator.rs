def5294bc680f2a2f8990f38b11295d535f88e96
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-04-29 12:38:32 +0200

    Rough implementation of ffi iterator - rust side

--- src/iterator.rs
+++ src/iterator.rs
@@ -36,7 +36,37 @@ impl Iterator for GoIter {
     type Item = StdResult<KV>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // TODO
-        None
+        let next_db = match self.vtable.next_db {
+            Some(f) => f,
+            // TODO: return None here???
+            None => return Some(Err(generic_err("iterator vtable not set"))),
+        };
+
+        let mut key_buf = Buffer::default();
+        let mut value_buf = Buffer::default();
+        let go_result: GoResult = (next_db)(
+            self.state,
+            &mut key_buf as *mut Buffer,
+            &mut value_buf as *mut Buffer,
+        )
+        .into();
+        if !go_result.is_ok() {
+            return Some(Err(generic_err(format!(
+                "Go {}: iterator.next()",
+                go_result
+            ))));
+        }
+
+        // TODO: Check if key is null, return none, otherwise, make KV
+        let okey = unsafe { key_buf.read() };
+        match okey {
+            Some(key) => {
+                // TODO: return error not unwrap??
+                let value = unsafe { value_buf.read().unwrap() };
+                let kv = (key.to_vec(), value.to_vec());
+                Some(Ok(kv))
+            }
+            None => None,
+        }
     }
 }
