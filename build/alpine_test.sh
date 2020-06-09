fc029a73493d0f400adc3c6a7b8fbd028ce15dfe
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-06-09 17:13:05 +0200

    We can build libgo_cosmwasm.so for alpine

--- build/alpine_test.sh
+++ build/alpine_test.sh
@@ -3,18 +3,33 @@
 #IMAGE=rust:alpine3.11
 IMAGE=demo-alpine:latest
 
+# FOR DEBUGGING
 #docker run -it -v $(pwd)/..:/code -w /code \
 #  --mount type=volume,source="gocosmwasm_alpine_cache",target=/code/target \
 #  ${IMAGE} /bin/sh
 
+# ENSURE TESTS RUN
+#docker run -v $(pwd)/..:/code -w /code \
+#  --mount type=volume,source="gocosmwasm_alpine_cache",target=/code/target \
+#  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+#  ${IMAGE} cargo test
+
+# BUILD ALPINE DLL
 docker run -v $(pwd)/..:/code -w /code \
   --mount type=volume,source="gocosmwasm_alpine_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   ${IMAGE} cargo build --release --features backtraces
 
+# VIEW THE DATA
 docker run -v $(pwd)/..:/code -w /code \
   --mount type=volume,source="gocosmwasm_alpine_cache",target=/code/target \
-  ${IMAGE} ls target target/release
+  ${IMAGE} ls target/release
+
+# COPY IT OUT
+docker run -v $(pwd)/..:/code -w /code \
+  --mount type=volume,source="gocosmwasm_alpine_cache",target=/code/target \
+  ${IMAGE} cp /code/target/release//libgo_cosmwasm.a /code/api
+
 
 ## for wasmer
 #docker run -v $(pwd):/code -w /code \
