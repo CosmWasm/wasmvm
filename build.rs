517f350ef1e8663f4da572dac3fe96dd0e1e8f34
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-06-26 14:28:11 +0200

    Rust test to demo CPU loop issue

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 use std::env;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::generate(crate_dir)
-        .expect("Unable to generate bindings")
-        .write_to_file("./api/bindings.h");
+        cbindgen::generate(crate_dir)
+            .expect("Unable to generate bindings")
+            .write_to_file("./api/bindings.h");
 }
