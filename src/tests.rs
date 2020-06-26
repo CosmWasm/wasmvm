4d00c16727c535d2d15192f5b6fd06f3b9185e6b
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-06-26 14:39:03 +0200

    Passing test for handle_cpu_loop showing CosmCache is the issue

--- src/tests.rs
+++ src/tests.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 use tempfile::TempDir;
 
 use cosmwasm_std::{coins, HumanAddr};
-use cosmwasm_vm::testing::{mock_dependencies, mock_env};
+use cosmwasm_vm::testing::{mock_dependencies, mock_env, mock_instance_with_gas_limit};
 use cosmwasm_vm::{call_handle_raw, call_init_raw, features_from_csv, to_vec, CosmCache};
 
 static CONTRACT: &[u8] = include_bytes!("../api/testdata/hackatom.wasm");
@@ -28,8 +28,9 @@ fn make_init_msg() -> (InitMsg, HumanAddr) {
     )
 }
 
+#[ignore]
 #[test]
-fn handle_cpu_loop() {
+fn handle_cpu_loop_with_cache() {
     let deps = mock_dependencies(20, &[]);
     let gas_limit = 2_000_000u64;
 
@@ -61,4 +62,28 @@ fn handle_cpu_loop() {
     assert!(res.is_err());
     assert_eq!(instance.get_gas_left(), 0);
     instance.recycle();
+}
+
+#[test]
+fn handle_cpu_loop_no_cache() {
+    let gas_limit = 2_000_000u64;
+    let mut instance = mock_instance_with_gas_limit(CONTRACT, gas_limit);
+
+    // init
+    let (init_msg, creator) = make_init_msg();
+    let env = mock_env(&instance.api, creator.as_str(), &coins(1000, "cosm"));
+    let raw_msg = to_vec(&init_msg).unwrap();
+    let raw_env = to_vec(&env).unwrap();
+    let res = call_init_raw(&mut instance, &raw_env, &raw_msg);
+    let gas_used = gas_limit - instance.get_gas_left();
+    println!("Init used gas: {}", gas_used);
+    res.unwrap();
+
+    // handle
+    let raw_msg = r#"{"cpu_loop":{}}"#;
+    let res = call_handle_raw(&mut instance, &raw_env, raw_msg.as_bytes());
+    let gas_used = gas_limit - instance.get_gas_left();
+    println!("Handle used gas: {}", gas_used);
+    assert!(res.is_err());
+    assert_eq!(instance.get_gas_left(), 0);
 }
