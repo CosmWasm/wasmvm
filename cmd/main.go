410a519a92bf394ec0e06d5e9271b9a6ddb92b75
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-04 18:39:45 +0200

    Fix one more place

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ func main() {
 	fmt.Println("Loaded!")
 
 	os.MkdirAll("tmp", 0755)
-	wasmer, err := wasm.NewWasmer("tmp", "staking", 0)
+	wasmer, err := wasm.NewWasmer("tmp", "staking")
 	if err != nil {
 		panic(err)
 	}
