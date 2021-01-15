2a2255f30770e0e922f80028f7b3d90f50193bc8
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-01-15 11:10:50 +0100

    Add new query variants

--- types/queries.go
+++ types/queries.go
@@ -81,10 +81,12 @@ func ToQuerierResult(response []byte, err error) QuerierResult {
 // QueryRequest is an rust enum and only (exactly) one of the fields should be set
 // Should we do a cleaner approach in Go? (type/data?)
 type QueryRequest struct {
-	Bank    *BankQuery      `json:"bank,omitempty"`
-	Custom  json.RawMessage `json:"custom,omitempty"`
-	Staking *StakingQuery   `json:"staking,omitempty"`
-	Wasm    *WasmQuery      `json:"wasm,omitempty"`
+	Bank     *BankQuery      `json:"bank,omitempty"`
+	Custom   json.RawMessage `json:"custom,omitempty"`
+	Ibc      *IbcQuery       `json:"ibc,omitempty"`
+	Staking  *StakingQuery   `json:"staking,omitempty"`
+	Stargate *StargateQuery  `json:"stargate,omitempty"`
+	Wasm     *WasmQuery      `json:"wasm,omitempty"`
 }
 
 type BankQuery struct {
@@ -109,6 +111,25 @@ type AllBalancesQuery struct {
 // AllBalancesResponse is the expected response to AllBalancesQuery
 type AllBalancesResponse struct {
 	Amount Coins `json:"amount"`
+}
+
+type IbcQuery struct {
+	PortId       *PortIdQuery       `json:"port_id,omitempty"`
+	ListChannels *ListChannelsQuery `json:"list_channels,omitempty"`
+	Channel      *ChannelQuery      `json:"channel,omitempty"`
+}
+
+type PortIdQuery struct{}
+
+type ListChannelsQuery struct {
+	// optional argument
+	PortId string `json:"port_id,omitempty"`
+}
+
+type ChannelQuery struct {
+	// optional argument
+	PortId    string `json:"port_id,omitempty"`
+	ChannelId string `json:"channel_id"`
 }
 
 type StakingQuery struct {
@@ -221,6 +242,23 @@ type FullDelegation struct {
 
 type BondedDenomResponse struct {
 	Denom string `json:"denom"`
+}
+
+// A Stargate query encoded the same way as abci_query, with path and protobuf encoded Data.
+// The format is defined in [ADR-21](https://github.com/cosmos/cosmos-sdk/blob/master/docs/architecture/adr-021-protobuf-query-encoding.md)
+// The response is also protobuf encoded. The caller is responsible for compiling the proper protobuf definitions
+type StargateQuery struct {
+	// this is the fully qualified service path used for routing,
+	// eg. custom/cosmos_sdk.x.bank.v1.Query/QueryBalance
+	Path string `json:"path"`
+	// this is the expected protobuf message type (not any), binary encoded
+	Data []byte `json:"data"`
+}
+
+// This is the protobuf response, binary encoded.
+// The caller is responsible for knowing how to parse.
+type StargateResponse struct {
+	Response []byte `json:"response"`
 }
 
 type WasmQuery struct {
