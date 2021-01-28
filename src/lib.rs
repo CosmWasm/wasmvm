713933c98b6e13c2aaa8df20cc42826ca9eaea70
Simon Warta <simon@warta.it>
2021-01-28 15:41:39 +0100

    Make clippy happy

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(feature = "backtraces", feature(backtrace))]
-#![allow(clippy::not_unsafe_ptr_arg_deref)]
+#![allow(clippy::not_unsafe_ptr_arg_deref, clippy::missing_safety_doc)]
 
 mod api;
 mod args;
