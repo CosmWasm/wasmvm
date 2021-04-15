f2743006239c9ed7350e23356fdebd0c3ec88fed
Simon Warta <simon@warta.it>
2021-04-15 15:35:06 +0200

    Format script

--- api/testdata/download_releases.sh
+++ api/testdata/download_releases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
-command -v shellcheck > /dev/null && shellcheck "$0"
+command -v shellcheck >/dev/null && shellcheck "$0"
 
 if [ $# -ne 1 ]; then
   echo "Usage: ./download_releases.sh RELEASE_TAG"
@@ -9,7 +9,7 @@ fi
 
 tag="$1"
 
-for contract in hackatom queue reflect ibc_reflect ; do
+for contract in hackatom queue reflect ibc_reflect; do
   url="https://github.com/CosmWasm/cosmwasm/releases/download/$tag/$contract.wasm"
   echo "Downloading $url ..."
   wget -O "$contract.wasm" "$url"
