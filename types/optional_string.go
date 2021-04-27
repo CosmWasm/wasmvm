82e7206a4004dca60a7d148167b3bb5a88cb1c18
Simon Warta <simon@warta.it>
2021-04-27 09:46:24 +0200

    Create OptionalString type

---
+++ types/optional_string.go
@@ -0,0 +1,69 @@
+package types
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// OptionalString is a type that is able to represent JSON's null or string.
+// It allows us to differentiate between a null field and an empty string.
+// The corresponding Rust type is Option<String>.
+// If you want to treat null the same way as the empty string, use .String()
+// which maps the two to an empty string.
+type OptionalString struct {
+	Set bool
+	/// Value is the string value when Set is true. When Set is unset, this field must be ignored.
+	Value string
+}
+
+func NewOptionalStringUnset() OptionalString {
+	return OptionalString{
+		Set:   false,
+		Value: "don't use me",
+	}
+}
+
+func NewOptionalStringSet(value string) OptionalString {
+	return OptionalString{
+		Set:   true,
+		Value: value,
+	}
+}
+
+// String converts the OptionalString to a string by mapping unset
+// to an empty string. Use this when you not need the differentiation anymore.
+func (os OptionalString) String() string {
+	if os.Set == false {
+		return ""
+	} else {
+		return os.Value
+	}
+}
+
+// MarshalJSON encodes a set OptionalString to a JSON string and an unset OptionalString to a JSON null
+func (in OptionalString) MarshalJSON() ([]byte, error) {
+	if in.Set == false {
+		return []byte("null"), nil
+	} else {
+		return json.Marshal(in.Value)
+	}
+}
+
+// UnmarshalJSON decodes a JSON string to a set OptionalString and null to an unset OptionalString
+func (out *OptionalString) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		*out = NewOptionalStringUnset()
+		return nil
+	}
+
+	if data[0] == '"' {
+		var value string
+		if err := json.Unmarshal(data, &value); err != nil {
+			return err
+		}
+		*out = NewOptionalStringSet(value)
+		return nil
+	}
+
+	return errors.New("Unexpted JSON type found. Must be null or string.")
+}
---
+++ types/optional_string_test.go
@@ -0,0 +1,73 @@
+package types
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestOptionalStringDefaultValue(t *testing.T) {
+	var os OptionalString
+	assert.Equal(t, false, os.Set)
+}
+
+func TestNewOptionalString(t *testing.T) {
+	os := NewOptionalStringSet("abc")
+	assert.Equal(t, "abc", os.String())
+
+	os = NewOptionalStringSet("")
+	assert.Equal(t, "", os.String())
+
+	os = NewOptionalStringUnset()
+	assert.Equal(t, "", os.String())
+}
+
+func TestOptionalStringToString(t *testing.T) {
+	os := NewOptionalStringSet("abc")
+	assert.Equal(t, OptionalString{Set: true, Value: "abc"}, os)
+
+	os = NewOptionalStringSet("")
+	assert.Equal(t, OptionalString{Set: true, Value: ""}, os)
+
+	os = NewOptionalStringUnset()
+	assert.Equal(t, false, os.Set)
+}
+
+func TestOptionalStringMarshalJSON(t *testing.T) {
+	alice := NewOptionalStringSet("alice")
+	bytes, err := json.Marshal(alice)
+	require.NoError(t, err)
+	assert.Equal(t, []byte("\"alice\""), bytes)
+
+	empty := NewOptionalStringSet("")
+	bytes, err = json.Marshal(empty)
+	require.NoError(t, err)
+	assert.Equal(t, []byte("\"\""), bytes)
+
+	unset := NewOptionalStringUnset()
+	bytes, err = json.Marshal(unset)
+	require.NoError(t, err)
+	assert.Equal(t, []byte("null"), bytes)
+}
+
+func TestOptionalStringUnmarshalJSON(t *testing.T) {
+	var os OptionalString
+
+	err := json.Unmarshal([]byte("null"), &os)
+	require.NoError(t, err)
+	assert.Equal(t, false, os.Set)
+
+	err = json.Unmarshal([]byte("\"\""), &os)
+	require.NoError(t, err)
+	assert.Equal(t, OptionalString{Set: true, Value: ""}, os)
+
+	err = json.Unmarshal([]byte("\"abc\""), &os)
+	require.NoError(t, err)
+	assert.Equal(t, OptionalString{Set: true, Value: "abc"}, os)
+
+	err = json.Unmarshal([]byte("123"), &os)
+	// TODO: Use ErrorContains once released (https://github.com/stretchr/testify/commit/6990a05d54)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "Unexpted JSON type")
+}
