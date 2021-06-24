c0207247e11ce43d87686e8902661b98cbf7e2f3
Tomasz Kurcz <uint@lavabit.com>
2021-06-24 14:19:08 +0200

    Doc update

--- lib.go
+++ lib.go
@@ -41,7 +41,7 @@ type VM struct {
 // `memoryLimit` is the memory limit of each contract execution (in MiB)
 // `printDebug` is a flag to enable/disable printing debug logs from the contract to STDOUT. This should be false in production environments.
 // `cacheSize` sets the size in MiB of an in-memory cache for e.g. module caching. Set to 0 to disable.
-// `deserCost` sets the gas cost of deserializing one kB of data.
+// `deserCost` sets the gas cost of deserializing one byte of data.
 func NewVM(dataDir string, supportedFeatures string, memoryLimit uint32, printDebug bool, cacheSize uint32) (*VM, error) {
 	cache, err := api.InitCache(dataDir, supportedFeatures, cacheSize, memoryLimit)
 	if err != nil {
