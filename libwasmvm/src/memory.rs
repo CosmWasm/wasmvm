f6b35a12e4f6c8fa7f832ce2b3cb6c0d7369289f
Simon Warta <simon@warta.it>
2021-10-27 14:02:35 +0200

    Add test unmanaged_vector_new_works

--- libwasmvm/src/memory.rs
+++ libwasmvm/src/memory.rs
@@ -307,6 +307,30 @@ mod test {
     }
 
     #[test]
+    fn unmanaged_vector_new_works() {
+        // With data
+        let x = UnmanagedVector::new(Some(vec![0x11, 0x22]));
+        assert_eq!(x.is_none, false);
+        assert_ne!(x.ptr as usize, 0);
+        assert_eq!(x.len, 2);
+        assert_eq!(x.cap, 2);
+
+        // Empty data
+        let x = UnmanagedVector::new(Some(vec![]));
+        assert_eq!(x.is_none, false);
+        assert_eq!(x.ptr as usize, 0x01); // We probably don't get any guarantee for this, but good to know where the 0x01 marker pointer can come from
+        assert_eq!(x.len, 0);
+        assert_eq!(x.cap, 0);
+
+        // None
+        let x = UnmanagedVector::new(None);
+        assert_eq!(x.is_none, true);
+        assert_eq!(x.ptr as usize, 0);
+        assert_eq!(x.len, 0);
+        assert_eq!(x.cap, 0);
+    }
+
+    #[test]
     fn unmanaged_vector_is_some_works() {
         let x = UnmanagedVector::new(Some(vec![0x11, 0x22]));
         assert_eq!(x.is_some(), true);
