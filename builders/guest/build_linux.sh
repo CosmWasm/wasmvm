cafc554d67cc35f4f03203e9ef237ee3751a2985
Simon Warta <simon@warta.it>
2022-01-02 11:04:15 +0100

    Harden bash scripts

--- builders/guest/build_linux.sh
+++ builders/guest/build_linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -o errexit -o nounset -o pipefail
 
 # See https://github.com/CosmWasm/wasmvm/issues/222#issuecomment-880616953 for two approaches to
 # enable stripping through cargo (if that is desired).
--- builders/guest/build_windows.sh
+++ builders/guest/build_windows.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -o errexit -o nounset -o pipefail
 
 # ref: https://www.reddit.com/r/rust/comments/5k8uab/crosscompiling_from_ubuntu_to_windows_with_rustup/
 
