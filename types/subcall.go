735b405292dd940fb9534fdb906e639dad529e47
Simon Warta <simon@warta.it>
2022-05-02 12:05:05 +0200

    Move types Event/Events to generic message and response types

--- types/subcall.go
+++ types/subcall.go
@@ -76,34 +76,3 @@ type SubcallResponse struct {
 	Events Events `json:"events"`
 	Data   []byte `json:"data,omitempty"`
 }
-
-// Events must encode empty array as []
-type Events []Event
-
-// MarshalJSON ensures that we get [] for empty arrays
-func (e Events) MarshalJSON() ([]byte, error) {
-	if len(e) == 0 {
-		return []byte("[]"), nil
-	}
-	var raw []Event = e
-	return json.Marshal(raw)
-}
-
-// UnmarshalJSON ensures that we get [] for empty arrays
-func (e *Events) UnmarshalJSON(data []byte) error {
-	// make sure we deserialize [] back to null
-	if string(data) == "[]" || string(data) == "null" {
-		return nil
-	}
-	var raw []Event
-	if err := json.Unmarshal(data, &raw); err != nil {
-		return err
-	}
-	*e = raw
-	return nil
-}
-
-type Event struct {
-	Type       string          `json:"type"`
-	Attributes EventAttributes `json:"attributes"`
-}
