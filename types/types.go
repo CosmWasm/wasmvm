9825f43d91b228c122e6d9b1d4b63ad15850fdd0
Christoph Otter <chris@confio.gmbh>
2023-05-31 16:18:46 +0200

    Use simplified pagination type

--- types/types.go
+++ types/types.go
@@ -94,24 +94,15 @@ type DenomUnit struct {
 	Aliases []string `json:"aliases"`
 }
 
-// Replicating the PageRequest type for pagination from the cosmos-sdk
+// Simplified version of the cosmos-sdk PageRequest type
 type PageRequest struct {
 	// key is a value returned in PageResponse.next_key to begin
 	// querying the next page most efficiently. Only one of offset or key
 	// should be set.
 	Key []byte `json:"key"`
-	// offset is a numeric offset that can be used when key is unavailable.
-	// It is less efficient than using key. Only one of offset or key should
-	// be set.
-	Offset uint64 `json:"offset"`
 	// limit is the total number of results to be returned in the result page.
 	// If left empty it will default to a value to be set by each app.
 	Limit uint64 `json:"limit"`
-	// count_total is set to true  to indicate that the result set should include
-	// a count of the total number of items available for pagination in UIs.
-	// count_total is only respected when offset is used. It is ignored when key
-	// is set.
-	CountTotal bool `json:"count_total"`
 	// reverse is set to true if results are to be returned in the descending order.
 	//
 	// Since: cosmos-sdk 0.43
