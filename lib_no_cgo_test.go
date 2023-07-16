d0d79cc36fb9069d8418017074ec927cb2bf6c2d
Simon Warta <simon@warta.it>
2023-07-16 11:34:30 +0200

    Fix some linting in tests

--- lib_no_cgo_test.go
+++ lib_no_cgo_test.go
@@ -9,24 +9,24 @@ import (
 
 func TestCreateChecksum(t *testing.T) {
 	// nil
-	checksum, err := CreateChecksum(nil)
+	_, err := CreateChecksum(nil)
 	require.ErrorContains(t, err, "nil or empty")
 
 	// empty
-	checksum, err = CreateChecksum([]byte{})
+	_, err = CreateChecksum([]byte{})
 	require.ErrorContains(t, err, "nil or empty")
 
 	// short
-	checksum, err = CreateChecksum([]byte("\x00\x61\x73"))
+	_, err = CreateChecksum([]byte("\x00\x61\x73"))
 	require.ErrorContains(t, err, " shorter than 4 bytes")
 
 	// Wasm blob returns correct hash
 	// echo "(module)" > my.wat && wat2wasm my.wat && hexdump -C my.wasm && sha256sum my.wasm
-	checksum, err = CreateChecksum([]byte("\x00\x61\x73\x6d\x01\x00\x00\x00"))
+	checksum, err := CreateChecksum([]byte("\x00\x61\x73\x6d\x01\x00\x00\x00"))
 	require.NoError(t, err)
 	require.Equal(t, types.ForceNewChecksum("93a44bbb96c751218e4c00d479e4c14358122a389acca16205b1e4d0dc5f9476"), checksum)
 
 	// Text file fails
-	checksum, err = CreateChecksum([]byte("Hello world"))
+	_, err = CreateChecksum([]byte("Hello world"))
 	require.ErrorContains(t, err, "do not not start with Wasm magic number")
 }
