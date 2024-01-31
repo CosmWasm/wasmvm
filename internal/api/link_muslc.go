39570962d866c590aab1552a2cbfe755d2b67e6f
Christoph Otter <chris@confio.gmbh>
2024-01-31 17:47:07 +0100

    Link against platform specific libwasmvm_muslc

--- internal/api/link_muslc.go
+++
@@ -1,8 +0,0 @@
-//go:build linux && muslc && !sys_wasmvm
-
-package api
-
-// TODO: once we switch to builders 0018+, split this linking statement in x86_64 and arm64 like we do with the glibc case
-
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lwasmvm_muslc
-import "C"
