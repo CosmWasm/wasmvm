069c8d9e05bc57da0d13530a246df2e78820c03d
Simon Warta <simon@warta.it>
2024-07-02 16:34:42 +0200

    Let builders target /target

--- builders/guest/build_muslc.sh
+++ builders/guest/build_muslc.sh
@@ -2,16 +2,17 @@
 set -e # Note we are not using bash here but the Alpine default shell
 
 export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
+export TARGET_DIR="/target" # write to /target in the guest's file system to avoid writing to the host
 
 # No stripping implemented (see https://github.com/CosmWasm/wasmvm/issues/222#issuecomment-2260007943).
 
 echo "Starting aarch64-unknown-linux-musl build"
 export CC=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc
-cargo build --release --target aarch64-unknown-linux-musl --example wasmvmstatic
+cargo build --release --target-dir="$TARGET_DIR" --target aarch64-unknown-linux-musl --example wasmvmstatic
 unset CC
 
 echo "Starting x86_64-unknown-linux-musl build"
-cargo build --release --target x86_64-unknown-linux-musl --example wasmvmstatic
+cargo build --release --target-dir="$TARGET_DIR" --target x86_64-unknown-linux-musl --example wasmvmstatic
 
-cp target/aarch64-unknown-linux-musl/release/examples/libwasmvmstatic.a artifacts/libwasmvm_muslc.aarch64.a
-cp target/x86_64-unknown-linux-musl/release/examples/libwasmvmstatic.a artifacts/libwasmvm_muslc.x86_64.a
+cp "$TARGET_DIR/aarch64-unknown-linux-musl/release/examples/libwasmvmstatic.a" artifacts/libwasmvm_muslc.aarch64.a
+cp "$TARGET_DIR/x86_64-unknown-linux-musl/release/examples/libwasmvmstatic.a" artifacts/libwasmvm_muslc.x86_64.a
