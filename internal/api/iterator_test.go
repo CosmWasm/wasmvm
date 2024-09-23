de105920a01e0048393317a570c28d1fe0bdd3a2
Christoph Otter <chris@confio.gmbh>
2024-09-23 17:28:44 +0700

    Fix tests

--- internal/api/iterator_test.go
+++ internal/api/iterator_test.go
@@ -265,7 +265,7 @@ func TestQueueIteratorLimit(t *testing.T) {
 	var gasLimit uint64
 
 	// Open 5000 iterators
-	gasLimit = 2 * TESTING_GAS_LIMIT
+	gasLimit = 3 * TESTING_GAS_LIMIT
 	gasMeter := NewMockGasMeter(gasLimit)
 	igasMeter := types.GasMeter(gasMeter)
 	store := setup.Store(gasMeter)
@@ -279,7 +279,7 @@ func TestQueueIteratorLimit(t *testing.T) {
 	require.Equal(t, `{}`, string(qres.Ok))
 
 	// Open 35000 iterators
-	gasLimit = TESTING_GAS_LIMIT * 7
+	gasLimit = TESTING_GAS_LIMIT * 8
 	gasMeter = NewMockGasMeter(gasLimit)
 	igasMeter = types.GasMeter(gasMeter)
 	store = setup.Store(gasMeter)
