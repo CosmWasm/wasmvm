5c3a9225d314260d20990c91bfb7652169382696
Christoph Otter <chris@confio.gmbh>
2024-09-23 17:37:14 +0700

    Fix rust test

--- libwasmvm/src/cache.rs
+++ libwasmvm/src/cache.rs
@@ -839,7 +839,7 @@ mod tests {
         assert_eq!(elements_memory_cache, 0);
         assert_approx_eq!(
             size_pinned_memory_cache,
-            2282344,
+            2500000,
             "0.2",
             "size_pinned_memory_cache: {size_pinned_memory_cache}"
         );
