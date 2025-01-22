800c506e6d58cc7ec4b7a1030309fd305f97e63e
Jan Woźniak <jnwznk@gmail.com>
2025-01-22 18:11:08 +0100

    Add new EurekaMsg

---
+++ types/eureka.go
@@ -0,0 +1,16 @@
+package types
+
+// EurekaPayload defines a single packet sent in the EurekaSendPacketMsg
+//
+// Payload value should be encoded in a format defined by the channel version,
+// and the module on the other side should know how to parse this.
+type EurekaPayload struct {
+	// The port id on the chain where the packet is sent to (external chain).
+	DestinationPort string `json:"destination_port"`
+	// Version of the receiving contract.
+	Version string `json:"version"`
+	// Encoding used to serialize the Value.
+	Encoding string `json:"encoding"`
+	// Encoded payload data
+	Value []byte `json:"value"`
+}
