d022fa26de867100cae144827b228d9f9bc6e57b
Christoph Otter <chris@confio.gmbh>
2025-05-09 11:17:37 +0200

    Add RawRange query

--- types/queries.go
+++ types/queries.go
@@ -373,6 +373,7 @@ type WasmQuery struct {
 	Raw          *RawQuery          `json:"raw,omitempty"`
 	ContractInfo *ContractInfoQuery `json:"contract_info,omitempty"`
 	CodeInfo     *CodeInfoQuery     `json:"code_info,omitempty"`
+	RawRange     *RawRangeQuery     `json:"raw_range,omitempty"`
 }
 
 // SmartQuery response is raw bytes ([]byte)
@@ -392,6 +393,23 @@ type RawQuery struct {
 type ContractInfoQuery struct {
 	// Bech32 encoded sdk.AccAddress of the contract
 	ContractAddr string `json:"contract_addr"`
+}
+
+type RawRangeQuery struct {
+	// The address of the contract to query
+	ContractAddr string `json:"contract_addr"`
+	// Inclusive start bound. This is the first key you would like to get data for.
+	//
+	// If `start` is lexicographically greater than or equal to `end`, an empty range is described, mo matter of the order.
+	Start *[]byte `json:"start,omitempty"`
+	// Exclusive end bound. This is the key after the last key you would like to get data for.
+	End *[]byte `json:"end,omitempty"`
+	// Maximum number of elements to return.
+	//
+	// Make sure to set a reasonable limit to avoid running out of memory or into the deserialization limits of the VM. Also keep in mind that these limitations depend on the full JSON size of the response type.
+	Limit uint16 `json:"limit"`
+	// The order in which you want to receive the key-value pairs.
+	Order string `json:"order"`
 }
 
 type ContractInfoResponse struct {
